7a76b1e5549962dd88914a732cd8275ed0a7f59d
SurabhiHolla <hollasurabhi.93@gmail.com>
2013-09-22 22:21:43 +0530

    Create main.m

---
+++ main.m
@@ -0,0 +1,18 @@
+#import"phone.h"
+#import"pboook.h"
+int main(int argc,char *argv[])
+{
+	NSAutoreleasePool *pool =[[NSAutoReleasePool alloc]init];
+	phoneCard *card=[[phoneCard alloc]init];
+	int phone;
+	char name[100],email[100];
+	NSLog(@"enter ur name");
+	scanf("%s",&name);
+	NSLog(@"enter emailid");
+	scanf("%s",&email);
+	NSLog(@"enter phonenumber");
+	scanf("%s",&phone);
+	NSString *name=[[NSString alloc]init];
+	NSString *email=[[NSString alloc]init];
+	
+}
