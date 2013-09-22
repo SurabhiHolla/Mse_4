8deab914d38f1a43dd8e2218fc4fa0eb4fb4ed34
SurabhiHolla <hollasurabhi.93@gmail.com>
2013-09-22 22:23:40 +0530

    Create phone.m

---
+++ phone.m
@@ -0,0 +1,12 @@
+#import"phone.h"
+@implemenation phonecard
+@synthesize phno,email,name;
+
+-(void) print
+{
+NSLog(@"name is %@"\n",name);
+NSLog(@"email id  is %@"\n",email);
+printf("phone number is %d",phno);
+}
+
+@end  
